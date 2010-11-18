61bf4c0586a0193eabf75a853730a97fd438420d
Dagfinn Ilmari Mannsåker <ilmari@ilmari.org>
2010-11-18 12:28:58 +0000

    Call ExtUtils::Depends from the built Makefile.PL, not when building it

--- inc/MultidimensionalMakeMaker.pm
+++ inc/MultidimensionalMakeMaker.pm
@@ -3,16 +3,12 @@ use Moose;
 
 extends 'Dist::Zilla::Plugin::MakeMaker::Awesome';
 
-use ExtUtils::Depends;
-
-override _build_WriteMakefile_args => sub {
+override _build_WriteMakefile_dump => sub {
     my ($self) = @_;
-    my $pkg = ExtUtils::Depends->new('multidimensional', 'B::Hooks::OP::Check');
+    my $str = super();
 
-    return +{
-        %{super()},
-        $pkg->get_makefile_vars,
-    };
+    $str =~ s/^(\s*)(.*)\n\);$/$1$2,\n$1do{ require ExtUtils::Depends; ExtUtils::Depends->new('multidimensional', 'B::Hooks::OP::Check')->get_makefile_vars }\n);/m;
+    return $str;
 };
 
 __PACKAGE__->meta->make_immutable;
